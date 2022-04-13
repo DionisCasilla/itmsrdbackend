2ed24487170218ca3538f7de54bdc77a9c462040
dionyscasilla <dionys.casilla@arcsoftware.us>
2022-04-12 22:26:52 -0400

    usuario key

--- app/routes/openseas.server.router.js
+++ app/routes/openseas.server.router.js
@@ -3,6 +3,7 @@ var shipping = require('../controllers/itmsshipping.server.controller');
 module.exports = function(app) {
     app.route('/itmsshipping/apptoken/:interID').get(shipping.apptoken);
     app.route('/itmsshipping/userlist').get(shipping.getUserDelivery);
+    app.route('/itmsshipping/userkeyvalidate/:userkey').get(shipping.getUserDeliverykey);
     app.route('/itmsshipping/config/:interID').get(shipping.getconfig);
     app.route('/itmsshipping/findForm/:formId/:type').get(shipping.findForm);
     app.route('/itmsshipping/saveForm').post(shipping.saveForm);
