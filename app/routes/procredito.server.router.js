d1d39c3a3617bf6a01ffd350dcb8050cc092f866
dionyscasilla <dionys.casilla@amadita.com>
2023-10-11 22:46:02 -0400

    hotfix envio de formularios

--- app/routes/procredito.server.router.js
+++ app/routes/procredito.server.router.js
@@ -2,7 +2,7 @@ var procredito = require('../controllers/procredito.server.controller.js');
 
 module.exports = function(app) {
     app.route('/procredito/loginapi').get(procredito.loginApi);
-    app.route('/procredito/submitForm').post(procredito.submitForm);
+    app.route('/procredito/submitForm').post(procredito.submitFormV2);
     app.route('/procredito/submitFormV2').post(procredito.submitFormV2);
     app.route('/procredito/bCedula/:cedula').get(procredito.bCedula);
     app.route('/procredito/loaddata').get(procredito.loaddata);
