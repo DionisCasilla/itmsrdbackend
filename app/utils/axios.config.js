58833683c43f2201669e039e0ab3ba8aeb3a3a20
dionyscasilla <dionys.casilla@amadita.com>
2023-10-11 22:07:08 -0400

    add

--- app/utils/axios.config.js
+++ app/utils/axios.config.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.setupAxios=()=>{
 
-    axios.defaults.baseURL= "https://services.viafirma.com/documents/api/v3/";
+    axios.defaults.baseURL= "https://documents.viafirma.com/documents/";
     axios.defaults.headers.Authorization ="Basic cHJvY3JlZGl0b19kb21pbmljYW5hOk1ENDNTQQ=="
        
 
